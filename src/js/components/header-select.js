ef81d790f5bd87a841d6ec58b3087fc7bef72c06
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-11-19 17:26:23 -0800

    updates code

--- src/js/components/header-select.js
+++ src/js/components/header-select.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 export default class HeaderSelect extends React.Component {
+  callChangeHeader(e) {
+    this.props.changeHeader(this.props.header.needed, e.target.value);
+  }
   render() {
     const { header, changeHeader, options } = this.props;
     return(
         <p>
         {header.needed}
-        <select selected={header.needed} >
+        <select onChange={this.callChangeHeader.bind(this)} selected={header.needed} >
          {options.map(option =>
              (<option value={option}>{option}</option>)
              )}
